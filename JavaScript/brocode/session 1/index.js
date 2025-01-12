14a107729a874f86bacc4e55cc4ccc9c61b7eedc
samp-7 <samavitoria7@gmail.com>
2025-01-12 19:17:01 -0300

    0

--- JavaScript/brocode/session 1/index.js
+++ JavaScript/brocode/session 1/index.js
@@ -1,10 +1,7 @@
-let online = true
-let BroOnline = "Is bro online?";
+let age = 17
+let fullName = "Veronika"
+let student = true;
 
-console.log(BroOnline)
-
-    if (online = true) {
-        console.log("Bro is online")
-    } else {
-        console.log("Bro is not online")
-    }
+document.getElementById("p1").textContent = `my name is ${fullName}`;
+document.getElementById("p2").textContent = `my age is ${age}`;
+document.getElementById("p3").textContent = `student: ${student}`;
