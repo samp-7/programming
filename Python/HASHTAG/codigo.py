6331a7698fcdc4007007897c85415f8ac3d7b165
samp-7 <samavitoria7@gmail.com>
2025-01-14 22:55:55 -0300

    oi

---
+++ Python/HASHTAG/codigo.py
@@ -0,0 +1,16 @@
+#vai tomar o cu piranha
+# escrever o passo a passo
+
+# Sistema: https://dlp.hashtagtreinamentos.com/python/intensivao/login
+
+import pyautogui
+
+# pyautogui.click
+pyautogui.press("win")
+
+pyautogui.write("chrome")
+
+pyautogui.press("enter")
+
+pyautogui.write("https://dlp.hashtagtreinamentos.com/python/intensivao/login")
+
