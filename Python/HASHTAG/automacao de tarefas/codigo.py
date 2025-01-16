3366ca4f8dfe195f296c2aae1b727ea062517ca9
samp-7 <samavitoria7@gmail.com>
2025-01-16 15:15:23 -0300

    cansei

--- Python/HASHTAG/automacao de tarefas/codigo.py
+++ Python/HASHTAG/automacao de tarefas/codigo.py
@@ -1,13 +1,21 @@
 # Sistema: https://dlp.hashtagtreinamentos.com/python/intensivao/login
 
 import pyautogui
+import time
 
-pyautogui.PAUSE = 2
+pyautogui.PAUSE = 4
 
 # APERTA WINDOWS, ESCREVE CHROME E ENTRA
 pyautogui.press('win')
-pyautogui.write('edge')
+pyautogui.write('google')
 pyautogui.press('enter')
 
-# pyautogui.write("https://dlp.hashtagtreinamentos.com/python/intensivao/login")
+pyautogui.write("https://dlp.hashtagtreinamentos.com/python/intensivao/login")
+pyautogui.press('enter')
+
+#dar intervalo especifico
+time.sleep(3)
+
+# clicar no lugar especifico
+pyautogui.click(x=586, y=155)
 
