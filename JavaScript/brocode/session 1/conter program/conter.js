37765baec15dc8ac9d737bda4326e3dd7fe7d73c
samp-7 <samavitoria7@gmail.com>
2025-01-21 17:17:17 -0300

    nossa

---
+++ JavaScript/brocode/session 1/conter program/conter.js
@@ -0,0 +1,20 @@
+const colocar = document.getElementById("colocar")
+const apagar = document.getElementById("apagar")
+const tirar = document.getElementById("tirar")
+const constLabel = document.getElementById("constLabel")
+let count = 0;
+
+colocar.onclick = function() {
+    count++;
+    constLabel.textContent = count;
+}
+
+tirar.onclick = function() {
+    count--;
+    constLabel.textContent = count;
+}
+
+apagar.onclick = function() {
+    count = 0;
+    constLabel.textContent = count;
+}
